9e0c405325aef92b87dfc7b8cd6a20856120c7ea
Vanisper <273266469@qq.com>
2025-07-04 14:23:15 +0800

    feat: 添加 FormCreate 示例组件并优化页面布局

--- src/pages-sub/form-create-demo/index.vue
+++ src/pages-sub/form-create-demo/index.vue
@@ -7,6 +7,51 @@
 }
 </route>
 
+<script lang="ts" setup>
+import formCreate from '@form-create/vant'
+import { showNotify } from 'vant'
+
+const formData = ref({})
+const formApi = ref(null)
+
+const formRules = ref([
+  {
+    type: 'input',
+    field: 'username',
+    title: '用户名',
+    value: '',
+  },
+  {
+    type: 'input',
+    field: 'password',
+    props: {
+      type: 'password',
+    },
+    title: '密码',
+    value: '',
+  },
+])
+
+function demoHandle() {
+  showNotify({
+    message: '测试\nvan 组件示例',
+    position: 'top',
+    type: 'warning',
+    duration: 800,
+  })
+}
+</script>
+
 <template>
-  <view>FormCreate demo</view>
+  <view class="flex flex-col gap-2 px-8 py-4">
+    <h1 class="text-center">
+      FormCreate demo
+    </h1>
+    <van-button round type="primary" plain size="small" block @click="demoHandle">
+      van 组件按需示例
+    </van-button>
+  </view>
+  <view>
+    <form-create v-model="formData" v-model:api="formApi" :rule="formRules" />
+  </view>
 </template>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -11,7 +11,6 @@
 </route>
 
 <script lang="ts" setup>
-import { Button as VanButton } from 'vant'
 import PLATFORM from '@/utils/platform'
 
 defineOptions({
@@ -70,9 +69,9 @@ function goDemo() {
       最好用的 uniapp 开发模板
     </view>
 
-    <van-button type="primary" plain block class="mb-4!" @click="goDemo">
+    <wd-button type="primary" plain block custom-class="mb-4!" @click="goDemo">
       go form-create-demo
-    </van-button>
+    </wd-button>
 
     <view class="m-auto mb-2 max-w-100 text-justify indent text-4">
       {{ description }}
